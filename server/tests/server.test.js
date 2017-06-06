0d48c7845d9db228ddc272e2be41cc848186ac23
Mariusz Rosa <m.rosa@ebros.pl>
2017-06-06 18:47:24 +0200

    test get

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: "zadanie 1"
+}, {
+    text: "zadanie 2"
+}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+      return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 
@@ -26,7 +34,7 @@ describe('post /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -48,10 +56,22 @@ describe('post /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
 
             });
     });
 });
+
+describe('GET /todos', () => {
+    it('return todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
+})
