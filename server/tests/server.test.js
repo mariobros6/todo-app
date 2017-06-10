ab581e8b02d809ba611a8d4c3e919c9d601bbe06
Mariusz Rosa <m.rosa@ebros.pl>
2017-06-10 17:00:44 +0200

    setup for heroku

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: "zadanie 1"
 }, {
+    _id: new ObjectID(),
     text: "zadanie 2"
 }];
 
@@ -74,4 +77,24 @@ describe('GET /todos', () => {
             })
             .end(done);
     });
-})
+});
+
+describe('GET /todos/:id', () => {
+    it('powinno zwrocic wybrany rekord', (done) => {
+        request(app)
+            .get('/todos/' + todos[0]._id.toHexString())
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text)
+            
+            })
+            .end(done);;
+    });
+
+    it('powinno zwrocic 404, gdy nie ma danych', (done) => {
+        request(app)
+            .get('/todos/' + (new ObjectID).toHexString())
+            .expect(404)
+            .end(done);
+    });
+});
