069f9dd79ab1d67546babf5510e2af429d47367f
Mariusz Rosa <m.rosa@ebros.pl>
2017-06-10 20:09:48 +0200

    patch update

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const {ObjectID} = require('mongodb');
-
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -54,7 +54,7 @@ app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id))
-    return res.status(400).send();
+    return res.status(404).send();
 
 
   Todo.findByIdAndRemove(id).then((todo) => {
@@ -69,6 +69,33 @@ app.delete('/todos/:id', (req, res) => {
     return res.status(400).send();
   });
 
+});
+
+app.patch('/todos/:id', (req, res) => {
+  
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    console.log(body);
+    if (!ObjectID.isValid(id))
+        return res.status(400).send();
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    console.log(body);
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo)
+            res.status(400).send();
+        console.log(todo);
+        res.send({todo});
+
+    }).catch((err) => {
+      res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
