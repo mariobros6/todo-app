c08dc67af12c266e6626ab654adc3a8861ad645a
Mariusz Rosa <m.rosa@ebros.pl>
2017-06-17 19:32:34 +0200

    authenticate

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ require('./config/config');
 
 const {ObjectID} = require('mongodb');
 const _ = require('lodash');
+const {authenticate} = require('./middleware/authenticate');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -40,6 +43,12 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.get('/todos', (req, res) => {
