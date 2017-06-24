39a372362546f13d74df7a4d5e6f969b4344fa5b
Mariusz Rosa <m.rosa@ebros.pl>
2017-06-24 14:13:02 +0200

    add user/login test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -199,10 +199,19 @@ describe('POST /users/', () => {
                 expect(res.body._id).toExist();
                 expect(res.body.email).toBe(email); 
                 console.log(res.body);
-                done();
+                
             })
             .end((err) => {
-                
+               if (err)
+                    return done(err);
+                User.findOne({email}).then((user) => {
+                    expect(user.email).toBe(email);
+                    expect(user.password).toNotBe(password);
+                    
+                    done();
+                }).catch((err) => {
+                    return done(err);
+                }); 
             });
     });
 
@@ -225,4 +234,51 @@ describe('POST /users/', () => {
             .end(done)
     });
    
+});
+
+describe('POST /users/login', () => {
+    it('should login user and return token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({email: users[1].email, password: users[1].password})
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist();
+                
+            })
+            .end((err, res) => {
+                if (err)
+                    return done(err);
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens[0]).toInclude({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    });
+                    done();
+                }).catch((err) => {
+                    return done(err);
+                });
+            });
+    });
+    it('should recejt user login', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({email:'aaa', password: 'pass'})
+            //.send({email: users[1].email, password: users[1].password})
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toNotExist();
+                
+            })
+            .end((err, res) => {
+                if (err)
+                    return done(err);
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((err) => {
+                    return done(err);
+                });
+            })
+    })
 })
